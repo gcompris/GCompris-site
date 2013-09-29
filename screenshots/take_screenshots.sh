b57bb196f8814155c69c23bbfb303d1fdef95c9e
Bruno Coudoin <bruno.coudoin@gcompris.net>
2013-09-29 22:09:18 +0200

    converted jpeg images to progressive.

--- screenshots/take_screenshots.sh
+++ screenshots/take_screenshots.sh
@@ -2,5 +2,5 @@
 # A command example I use to create the screenshots.
 # Move them in the screenshots directory with the name found
 # in their .xml.in menu name field
-xwd -name GCompris | convert - -quality 85 -resize 50% $1.jpg
+xwd -name GCompris | convert - -quality 85 -interlace line -resize 50% $1.jpg
 #convert -geometry 130x130 -quality 100 $1.jpg $1_small.jpg
