305b01f760063b2de0c6123fef8fd68213bd5e19
Bruno Coudoin <bruno.coudoin@gcompris.net>
2018-08-25 13:01:51 +0200

    addded railroad png, improved script
    added an optipng pass to reduce the png size.

--- screenshots_qt/small_middle_converter.sh
+++ screenshots_qt/small_middle_converter.sh
@@ -4,12 +4,28 @@
 # Run me before pushing to gcompris.net is not yet done or if a new screenshot
 # was added.
 
+# Requires optipng (apt install optipng)
+
 mkdir -p middle
 mkdir -p small
 for f in large/*.png
 do
     png=${f##*/}
-    echo "Processing $f"
-    convert $f -resize 50% middle/$png
-    convert $f -resize 25% small/$png
+
+    if [ ! -f middle/$png ]
+    then
+        echo "Processing middle $f"
+        convert $f -resize 50% middle/$png
+        optipng middle/$png
+    fi
+
+    if [ ! -f small/$png ]
+    then
+        echo "Processing small $f"
+        convert $f -resize 25% small/$png
+        optipng small/$png
+
+        # Also optimize the new source file
+        optipng large/$png
+    fi
 done
