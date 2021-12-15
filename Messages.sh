53a2b0b3ada46c900d7ba456093929939fa570c6
Timothée Giet <animtim@gmail.com>
2021-12-15 20:11:15 +0100

    add a parameter to convert the 2 last news to po

--- Messages.sh
+++ Messages.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 pybabel extract --add-comments=notes -F babel.cfg -o $podir/gcompris-net.pot .
-python3 ./tools/convertLastNewsToPo.py
+python3 ./tools/convertLastNewsToPo.py 2
